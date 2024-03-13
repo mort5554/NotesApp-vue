af3b9c1abf346f35eaf0ecb8e516d9679306e7a4
mort5554 <116391403+mort5554@users.noreply.github.com>
2024-03-13 22:05:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NotesApp-vue',
+  base: '/NotesApp-vue/',
   plugins: [
     vue(),
   ],
